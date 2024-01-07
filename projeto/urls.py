d64d96992a7591795506b3b2816d7e4a41250a7d
Dnstilt <denistakenaka@gmail.com>
2024-01-07 20:58:03 -0300

    "app views"

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from recipes.views import home, sobre, contato 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home), #Home
+    path('sobre/', sobre), #Sobre
+    path('contato/', contato), #Contato
 ]
