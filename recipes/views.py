9f3314f85c46fd7b2327de7f64c1fc30fb9ad995
Dnstilt <denistakenaka@gmail.com>
2024-01-08 00:04:26 -0300

    "CSS header"

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def home(request):
-    return HttpResponse("Home")
+    return render(request, "recipes\pages\home.html")
 
-def sobre(request):
-    return HttpResponse("Sobre")
-
-def contato(request):
-    return HttpResponse("Contato")
 
