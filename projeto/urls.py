1b0ca6d1c917adbe36492dd2d3e2d8637f1b28b2
Dnstilt <denistakenaka@gmail.com>
2024-02-18 23:47:58 -0300

    Fixing recipe content

--- projeto/urls.py
+++ projeto/urls.py
@@ -23,7 +23,8 @@ app_name = 'recipes'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("recipes.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+           static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+"""urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)"""
--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=65)
     
@@ -24,11 +25,12 @@ class Recipe(models.Model):
     cover = models.ImageField(
         upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete = models.SET_NULL, null= True
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
         )
     
     author = models.ForeignKey(
-        User, on_delete = models.SET_NULL, null= True
+        User, on_delete= models.SET_NULL, null= True
         )
     
     def __str__(self):
